562ad987aa6f739ec9e5a7f86555ba6eba746ea2
BrattKondrattenko <artkondratenko3@gmail.com>
2024-01-10 13:41:51 +0300

    realize pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,124 +1,130 @@
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
-import type { ColumnsType } from 'antd/es/table';
+import { useState, useEffect } from "react";
+import { Button, Space, Table, Tag } from "antd";
+import type { ColumnsType } from "antd/es/table";
+import axios from "axios";
+
+const LIMIT_LIST = 6;
+
+const getRateColor = (rate: number) => {
+    switch (parseInt(rate.toString())) {
+        case 4:
+            return "green";
+        case 3:
+            return "gold";
+        case 2:
+            return "volcano";
+        case 1:
+            return "red";
+        case 0:
+            return "#151719";
+        default:
+            throw new Error("unexpected rate");
+    }
+};
 
 interface DataType {
-    key: string;
-    name: string;
-    age: number;
-    address: string;
-    gender: string;
-    email: string;
-    tags: string[];
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    rating: number;
+    brand: string;
+    category: string;
 }
 
 const columns: ColumnsType<DataType> = [
     {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text) => <a>{text}</a>,
+        title: "Name",
+        dataIndex: "title",
+        key: "title",
     },
     {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
+        title: "Description",
+        dataIndex: "description",
+        key: "description",
     },
     {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
+        title: "Price",
+        dataIndex: "price",
+        key: "price",
+        render: (text) => <p>{text}$</p>,
     },
     {
-        title: 'Gender',
-        dataIndex: 'gender',
-        key: 'gender',
+        title: "Rating",
+        dataIndex: "rating",
+        key: "rating",
+        render: (text) => <Tag color={getRateColor(text)}>{text}</Tag>,
     },
     {
-        title: 'Email',
-        dataIndex: 'email',
-        key: 'email',
+        title: "Brand",
+        dataIndex: "brand",
+        key: "brand",
     },
     {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: (_, { tags }) => (
-            <>
-                {tags.map((tag) => {
-                    let color = tag.length > 5 ? 'geekblue' : 'green';
-                    if (tag === 'loser') {
-                        color = 'volcano';
-                    }
-                    return (
-                        <Tag color={color} key={tag}>
-                            {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </>
-        ),
+        title: "Category",
+        key: "category",
+        dataIndex: "category",
     },
     {
-        title: 'Action',
-        key: 'action',
+        title: "Action",
+        key: "action",
         render: (_, record) => (
             <Space size="middle">
-                <a>Invite {record.name}</a>
-                <a>Delete</a>
+                <a>Buy {record.title}</a>
             </Space>
         ),
     },
 ];
 
-const data: DataType[] = [
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        gender: 'male',
-        email: 'john@gmail.com',
-        tags: ['nice', 'developer'],
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        gender: 'male',
-        email: 'Green@gmail.com',
-        tags: ['loser'],
-    },
-    {
-        key: '3',
-        name: 'Anna Perry',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        gender: 'female',
-        email: 'Anna@gmail.com',
-        tags: ['cool', 'teacher'],
-    },
-    {
-        key: '4',
-        name: 'New Human Being Alex',
-        age: 32,
-        address: 'Moscow city',
-        gender: 'male',
-        email: 'moscow@hmail.com',
-        tags: ['cool', 'business'],
-    },
-    {
-        key: '5',
-        name: 'New Human Being Danilo',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        gender: 'female',
-        email: 'Anna@gmail.com',
-        tags: ['man', 'footbal'],
-    },
-];
+const App = () => {
+    const [page, setPage] = useState<number>(1);
+    const [totalItems, setTotalItems] = useState<number>(0);
+    const [dataSource, setDataSource] = useState<DataType[]>([]);
 
-const App: React.FC = () => <Table columns={columns} dataSource={data} />;
+    const getData = async (page: number, limit: number) => {
+        try {
+            const response = await axios.get(
+                `https://dummyjson.com/products?limit=${limit}&skip=${
+                    (page - 1) * limit
+                }&select=title,description,price,rating,brand,category`
+            );
+            setDataSource(response.data.products);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    useEffect(() => {
+        const setTotalCount = async () => {
+            try {
+                const response = await axios.get("https://dummyjson.com/products");
+                setTotalItems(response.data.total);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+        setTotalCount();
+    }, []);
+
+    useEffect(() => {
+        getData(page, LIMIT_LIST);
+    }, [page]);
+
+    return (
+        <>
+            <Table columns={columns} dataSource={dataSource} pagination={false} />
+            <Button onClick={() => setPage(page - 1)} disabled={page === 1}>
+                Назад
+            </Button>
+            <Button
+                onClick={() => setPage(page + 1)}
+                disabled={page * LIMIT_LIST >= totalItems}
+            >
+                Вперед
+            </Button>
+            <p>Текущая страница: {page}</p>
+        </>
+    );
+};
 
 export default App;
